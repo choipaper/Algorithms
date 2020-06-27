a5cbb32596a2bf409fcd922b2f0345dbddb1dd81
choipaper-ubuntu <choipaper.develop@gmail.com>
2020-06-27 16:20:56 -0700

    Add testing cpp file

---
+++ Test.cpp
@@ -0,0 +1,23 @@
+/**
+ * To compile any algorithm test case before submit it.
+ * 
+ * To complie: g++ Test.cpp -o testoutput
+ * 
+ */
+
+#include <iostream>
+#include <vector>
+
+// Test function
+
+// main
+int main()
+{
+    //test input
+    //test function
+    // test output
+    //std::cout << testCase << std::endl;
+    std::cout << "Test" << std::endl;
+
+    return 0;
+}
